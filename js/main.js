b6f61e17d42758b621fb9634b40b577ebad50b1d
paradoxalyty <paradoxalyty@gmail.com>
2021-10-07 23:18:52 +0200

    add media queries

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 let bg = document.getElementById('bg');
 let moon = document.getElementById('moon');
+let moonCenter = document.getElementById('moon-center');
 let mountain = document.getElementById('mountain');
 let road = document.getElementById('road');
 let text = document.getElementById('text');
 
 window.addEventListener('scroll', function () {
-  var value = window.scrollY;
+  let value = window.scrollY;
+  console.log(moon.style.left);
 
   bg.style.top = value * 0.5 + 'px';
   moon.style.left = value * 1 + 'px';
   moon.style.top = value * 1 + 'px';
+  moonCenter.style.left = value * 1 + 'px';
+  moonCenter.style.top = value * 1 + 'px';
   mountain.style.top = -value * 0.15 + 'px';
   road.style.top = value * 0.15 + 'px';
   text.style.top = value * 1 + 'px';
 });
+
+console.log(moon.style);
